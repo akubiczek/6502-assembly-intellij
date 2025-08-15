d7564a89da5fa7dd45092ab3c911e7458f1a9a25
Adam Kubiczek <adam@kubiczek.eu>
2025-08-15 18:33:21 +0200

    Upgrade to Gradle 9

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@ set -exu -o pipefail
 rm -Rf build src/main/gen src/main/resources/projectTemplates/*.zip
 (cd "templates/Generic 6502 Project" && zip --recurse-paths "../../src/main/resources/projectTemplates/Generic 6502 Project.zip" root0/ .idea/)
 ./gradlew generateAsmLexer generateAsmParser verifyPlugin buildPlugin
-./gradlew runPluginVerifier
